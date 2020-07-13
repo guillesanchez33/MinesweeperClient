087a9462e59bfc6868f45e412ad9b2dce8290e97
Guillermo Sanchez <geschess@gmail.com>
2020-07-12 22:28:48 -0300

    clean code

--- src/app/security/http.interceptor.ts
+++ src/app/security/http.interceptor.ts
@@ -15,26 +15,17 @@ export class MyHttpInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    console.log("1.intercept");
-
     var token: string = localStorage.getItem('sessionId');
     if (token == null ) {
         token = "";
     }
-    console.log("2.intercept>  token: " + token);
     request = request.clone({headers: request.headers.set('Accept', 'application/json, text/plain, */*')});
     request = request.clone({headers: request.headers.set('Content-Type', 'application/json')});
-    //if (token) {
-        request = request.clone({headers: request.headers.set('sessionId', token/*"13686d31-bb92-406a-bcf6-45aa23e6774c"*/)});
-    //}
-    console.log("3.intercept");
-
+    request = request.clone({headers: request.headers.set('sessionId', token)});
     return next.handle(request).pipe(
       map((event: HttpEvent<any>) => {
         if (event instanceof HttpResponse) {
           if (event.status === 200) {
-            //console.log(event.body);
             const body= JSON.parse(event.body);
             if(body.session){
                 localStorage.setItem('sessionId', body.session);
